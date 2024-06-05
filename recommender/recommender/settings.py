06fd66a27ea36018405097e570a3bee5cb30c561
VakaPrasanna <20BQ1A05O6@vvit.net>
2024-06-05 14:17:52 +0530

    created view

--- recommender/recommender/settings.py
+++ recommender/recommender/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'movierecommender.apps.MovierecommenderConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movierecommender.apps.MovierecommenderConfig',
 ]
 
 MIDDLEWARE = [
